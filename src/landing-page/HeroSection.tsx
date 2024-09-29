11ce24b103f32ce351cc03ca53621cd1b37c0d52
oseji <jiade1233@gmail.com>
2024-09-29 21:11:09 +0100

    added bg image in herosection

--- src/landing-page/HeroSection.tsx
+++ src/landing-page/HeroSection.tsx
@@ -3,6 +3,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 
 import logo from "../assets/Binge.svg";
+import bacg from "../assets/heroSection-bg.jpg";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -25,42 +26,52 @@ const HeroSection = () => {
 
   return (
     <section className="HeroSection">
-      <header>
-        <ul className="headerList">
-          <li>Movies</li>
-          <li>Series</li>
-          <li>Tv Shows</li>
-        </ul>
+      {/* <div className=" absolute w-full h-full top-0 left-0 bg-black"></div> */}
 
-        <img src={logo} alt="Binge Logo" />
+      <div
+        className=" p-5 absolute w-full h-full"
+        style={{
+          backgroundImage: `url("src/assets/heroSection-bg.jpg")`,
+          backgroundSize: "100% 100%",
+        }}
+      >
+        <header>
+          <ul className="headerList">
+            <li>Movies</li>
+            <li>Series</li>
+            <li>Tv Shows</li>
+          </ul>
 
-        <ul className="headerList">
-          <li>Choose your language</li>
-          <li>Sign up</li>
-        </ul>
-      </header>
+          <img src={logo} alt="Binge Logo" />
 
-      <div className=" text-center min-h-[100dvh] flex flex-col justify-center">
-        <h1 className=" text-center text-5xl font-semibold pb-2">
-          Welcome to Bin
-          <span className=" text-[#9B51E0] inline-block" ref={headingRef}>
-            ge!
-          </span>
-        </h1>
+          <ul className="headerList">
+            <li>Choose your language</li>
+            <li>Sign up</li>
+          </ul>
+        </header>
 
-        <p className="sectionSubHeading w-[435px] mx-auto">
-          Lorem ipsum dolor sit amet consectetur. Aenean augue e u bibendum
-          morbi.
-        </p>
+        <div className=" text-center min-h-[100dvh] flex flex-col justify-center">
+          <h1 className=" text-center text-5xl font-semibold pb-2">
+            Welcome to Bin
+            <span className=" text-[#9B51E0] inline-block" ref={headingRef}>
+              ge!
+            </span>
+          </h1>
 
-        <div className=" flex flex-col items-center gap-3 mt-5">
-          <button className="heroSectionBtns bg-[#9B51E0] rounded-md">
-            Wanna Join Binge? Sign Up
-          </button>
+          <p className="sectionSubHeading w-[435px] mx-auto">
+            Lorem ipsum dolor sit amet consectetur. Aenean augue e u bibendum
+            morbi.
+          </p>
 
-          <button className="heroSectionBtns bg-transparent text-[#9B51E0] outline-[#9B51E0] outline ">
-            Start 7-day Free Trial
-          </button>
+          <div className=" flex flex-col items-center gap-3 mt-5">
+            <button className="heroSectionBtns bg-[#9B51E0] rounded-md">
+              Wanna Join Binge? Sign Up
+            </button>
+
+            <button className="heroSectionBtns bg-transparent text-[#9B51E0] outline-[#9B51E0] outline ">
+              Start 7-day Free Trial
+            </button>
+          </div>
         </div>
       </div>
     </section>
