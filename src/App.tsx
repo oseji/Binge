2ac109e6b07b568d66a80710a4507060285bab46
oseji <jiade1233@gmail.com>
2024-10-10 16:39:37 +0100

    started working on registartion page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import { useRef, useEffect, useState } from "react";
+import { Route, Switch, Link } from "react-router-dom";
+
+import RegistrationPage from "./authentication-pages/RegistrationPage";
+
 import HeroSection from "./landing-page/HeroSection";
 import Favorites from "./landing-page/Favorites";
 import Features from "./landing-page/Features";
@@ -10,14 +15,10 @@ import closeMenu from "./assets/icons8-close.svg";
 
 import logo from "./assets/Binge.svg";
 
-import { useRef, useEffect, useState } from "react";
-
 function App() {
   const menuRef = useRef<HTMLImageElement>(null);
   const [menuToggled, setMenuToggled] = useState(false);
   const mainScreenRef = useRef<HTMLDivElement>(null);
-
-  const [loggedIn, setLoggedIn] = useState<boolean>(false);
 
   // menu toggle
   useEffect(() => {
@@ -38,63 +39,73 @@ function App() {
 
   return (
     <div className="App">
-      {!loggedIn && (
-        <div className="landingPage">
-          {/* header for tablets and desktops */}
-          <header className="bigScreenHeader">
-            <ul className="headerList">
-              <li>Movies</li>
-              <li>Series</li>
-              <li>Tv Shows</li>
-            </ul>
-
-            <img src={logo} alt="Binge Logo" className="headerLogo" />
-
-            <ul className="headerList">
-              <li>Choose your language</li>
-              <li>Sign up</li>
-            </ul>
-          </header>
-
-          {/* header for phones */}
-          <header className="smallScreenHeader ">
-            <div className=" flex flex-row justify-between items-start">
-              <img src={logo} alt="Binge Logo" className=" h-8" />
-
-              <img
-                src={menuToggled ? closeMenu : menu}
-                alt="menu image"
-                className=" h-8"
-                onClick={() => setMenuToggled((prev) => !prev)}
-              />
-            </div>
-
-            <div className="menuHidden " ref={menuRef}>
-              <ul className="flex flex-col items-start  gap-16 bg-black text-2xl uppercase ">
+      <Switch>
+        <Route exact path={"/"}>
+          <div className="landingPage">
+            {/* header for tablets and desktops */}
+            <header className="bigScreenHeader">
+              <ul className="headerList">
                 <li>Movies</li>
                 <li>Series</li>
                 <li>Tv Shows</li>
-                <li>Choose your language</li>
-                <li>Sign up</li>
               </ul>
+
+              <img src={logo} alt="Binge Logo" className="headerLogo" />
+
+              <ul className="headerList">
+                <li>Choose your language</li>
+                <Link to={"/RegistrationPage"}>
+                  <li>Sign up</li>
+                </Link>
+              </ul>
+            </header>
+
+            {/* header for phones */}
+            <header className="smallScreenHeader ">
+              <div className=" flex flex-row justify-between items-start">
+                <img src={logo} alt="Binge Logo" className=" h-8" />
+
+                <img
+                  src={menuToggled ? closeMenu : menu}
+                  alt="menu image"
+                  className=" h-8"
+                  onClick={() => setMenuToggled((prev) => !prev)}
+                />
+              </div>
+
+              <div className="menuHidden " ref={menuRef}>
+                <ul className="flex flex-col items-start  gap-16 bg-black text-2xl uppercase ">
+                  <li>Movies</li>
+                  <li>Series</li>
+                  <li>Tv Shows</li>
+                  <li>Choose your language</li>
+                  <Link to={"/RegistrationPage"}>
+                    <li>Sign up</li>
+                  </Link>
+                </ul>
+              </div>
+            </header>
+
+            <div ref={mainScreenRef}>
+              <HeroSection></HeroSection>
+
+              <Favorites></Favorites>
+
+              <Features></Features>
+
+              <Pricing></Pricing>
+
+              <Questions></Questions>
+
+              <Footer></Footer>
             </div>
-          </header>
-
-          <div ref={mainScreenRef}>
-            <HeroSection></HeroSection>
-
-            <Favorites></Favorites>
-
-            <Features></Features>
-
-            <Pricing></Pricing>
-
-            <Questions></Questions>
-
-            <Footer></Footer>
           </div>
-        </div>
-      )}
+        </Route>
+
+        <Route path={"/RegistrationPage"}>
+          <RegistrationPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/authentication-pages/RegistrationPage.tsx
+++ src/authentication-pages/RegistrationPage.tsx
@@ -1,13 +1,84 @@
+import BingeLogo from "../assets/registration logo.svg";
+
 const RegistrationPage = () => {
   return (
     <section id="registrationPage">
       <div
-        className=" absolute h-full w-full top-0 left-0"
+        className=" absolute h-full w-full top-0 left-0 bg-cover"
         style={{
           backgroundImage: 'url("/heroSection-bg.jpg")',
           backgroundSize: "100% 100%",
         }}
       ></div>
+
+      <form className="registrationForm">
+        <div className=" flex flex-col items-center">
+          <img src={BingeLogo} alt="Binge Logo" className=" h-10" />
+          <p className=" text-xl font-bold">Create an Account</p>
+        </div>
+
+        <div className=" flex flex-col gap-3 items-center text-sm">
+          <div className="inputGrp">
+            <label htmlFor="firstName">first name</label>
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              placeholder="Sandra"
+            />
+          </div>
+
+          <div className="inputGrp">
+            <label htmlFor="lastName">last name</label>
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              placeholder="Bloyd"
+            />
+          </div>
+
+          <div className="inputGrp">
+            <label htmlFor="phoneNumber">phone number</label>
+            <input
+              type="number"
+              name="phoneNumber"
+              id="name"
+              placeholder="+234 7024341178"
+            />
+          </div>
+
+          <div className="inputGrp">
+            <label htmlFor="emailAddress">email address</label>
+            <input
+              type="email"
+              name="emailAddress"
+              id="emailAddress"
+              placeholder="SandyB@gmail.com"
+            />
+          </div>
+
+          <div className="inputGrp">
+            <label htmlFor="password">password</label>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              placeholder="Sandra"
+            />
+          </div>
+
+          <div className="inputGrp">
+            <label htmlFor="confirmPassword">confirm password</label>
+            <input
+              type="text"
+              name="confirmPassword"
+              id="confirmPassword"
+              placeholder="Sandra"
+            />
+          </div>
+        </div>
+      </form>
     </section>
   );
 };
