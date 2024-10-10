2d425fbea85b8d161492b1c0b81877ab56c363fc
oseji <jiade1233@gmail.com>
2024-10-10 16:00:01 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ function App() {
   const [menuToggled, setMenuToggled] = useState(false);
   const mainScreenRef = useRef<HTMLDivElement>(null);
 
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+
   // menu toggle
   useEffect(() => {
     if (menuRef.current) {
@@ -36,61 +38,63 @@ function App() {
 
   return (
     <div className="App">
-      <div className="landingPage">
-        {/* header for tablets and desktops */}
-        <header className="bigScreenHeader">
-          <ul className="headerList">
-            <li>Movies</li>
-            <li>Series</li>
-            <li>Tv Shows</li>
-          </ul>
-
-          <img src={logo} alt="Binge Logo" className="headerLogo" />
-
-          <ul className="headerList">
-            <li>Choose your language</li>
-            <li>Sign up</li>
-          </ul>
-        </header>
-
-        {/* header for phones */}
-        <header className="smallScreenHeader ">
-          <div className=" flex flex-row justify-between items-start">
-            <img src={logo} alt="Binge Logo" className=" h-8" />
-
-            <img
-              src={menuToggled ? closeMenu : menu}
-              alt="menu image"
-              className=" h-8"
-              onClick={() => setMenuToggled((prev) => !prev)}
-            />
-          </div>
-
-          <div className="menuHidden " ref={menuRef}>
-            <ul className="flex flex-col items-start  gap-16 bg-black text-2xl uppercase ">
+      {!loggedIn && (
+        <div className="landingPage">
+          {/* header for tablets and desktops */}
+          <header className="bigScreenHeader">
+            <ul className="headerList">
               <li>Movies</li>
               <li>Series</li>
               <li>Tv Shows</li>
+            </ul>
+
+            <img src={logo} alt="Binge Logo" className="headerLogo" />
+
+            <ul className="headerList">
               <li>Choose your language</li>
               <li>Sign up</li>
             </ul>
+          </header>
+
+          {/* header for phones */}
+          <header className="smallScreenHeader ">
+            <div className=" flex flex-row justify-between items-start">
+              <img src={logo} alt="Binge Logo" className=" h-8" />
+
+              <img
+                src={menuToggled ? closeMenu : menu}
+                alt="menu image"
+                className=" h-8"
+                onClick={() => setMenuToggled((prev) => !prev)}
+              />
+            </div>
+
+            <div className="menuHidden " ref={menuRef}>
+              <ul className="flex flex-col items-start  gap-16 bg-black text-2xl uppercase ">
+                <li>Movies</li>
+                <li>Series</li>
+                <li>Tv Shows</li>
+                <li>Choose your language</li>
+                <li>Sign up</li>
+              </ul>
+            </div>
+          </header>
+
+          <div ref={mainScreenRef}>
+            <HeroSection></HeroSection>
+
+            <Favorites></Favorites>
+
+            <Features></Features>
+
+            <Pricing></Pricing>
+
+            <Questions></Questions>
+
+            <Footer></Footer>
           </div>
-        </header>
-
-        <div ref={mainScreenRef}>
-          <HeroSection></HeroSection>
-
-          <Favorites></Favorites>
-
-          <Features></Features>
-
-          <Pricing></Pricing>
-
-          <Questions></Questions>
-
-          <Footer></Footer>
         </div>
-      </div>
+      )}
     </div>
   );
 }
---
+++ src/authentication-pages/RegistrationPage.tsx
@@ -0,0 +1,15 @@
+const RegistrationPage = () => {
+  return (
+    <section id="registrationPage">
+      <div
+        className=" absolute h-full w-full top-0 left-0"
+        style={{
+          backgroundImage: 'url("/heroSection-bg.jpg")',
+          backgroundSize: "100% 100%",
+        }}
+      ></div>
+    </section>
+  );
+};
+
+export default RegistrationPage;
